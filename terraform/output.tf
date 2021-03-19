3da7a21c3db05a865131192ae0276ef5158e0fbf
momumomu <26238953+momumomu@users.noreply.github.com>
2021-03-19 17:55:00 +0900

    first_commit

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,3 @@
 output "api_gateway_url" {
   value = "${aws_api_gateway_deployment.slack_channel_rename_api_gateway.invoke_url}${aws_api_gateway_resource.slack_channel_rename_api_gateway.path}"
 }
-
-// output "iddd" {
-//   value = "${aws_api_gateway_resource.slack_channel_rename_api_gateway.id}"
-// }
-
-// output "pathh" {
-//   value = "${aws_api_gateway_resource.slack_channel_rename_api_gateway.path}"
-// }
